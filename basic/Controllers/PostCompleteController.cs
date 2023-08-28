dd950b16a3c3653cfa8f337c28ebd6dda75d6e00
Raju Gharti <gharti.rg@gmail.com>
2023-08-28 16:46:01 -0400

    implement stored procedure for post

--- basic/Controllers/PostCompleteController.cs
+++ basic/Controllers/PostCompleteController.cs
@@ -1,4 +1,6 @@
+using AutoMapper;
 using basic.Data.Repositories.Posts;
+using basic.Dtos;
 using basic.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -11,10 +13,12 @@ namespace basic.Controllers;
 public class PostCompleteController : ControllerBase
 {
     private readonly ICompletePostRepository _completePostRepository;
+    private readonly IMapper _mapper;
 
-    public PostCompleteController(ICompletePostRepository completePostRepository)
+    public PostCompleteController(ICompletePostRepository completePostRepository, IMapper mapper)
     {
         _completePostRepository = completePostRepository;
+        _mapper = mapper;
     }
 
     [HttpGet("posts")]
@@ -32,7 +36,7 @@ public class PostCompleteController : ControllerBase
     }
 
     [HttpGet("{postId:int:min(1)}")]
-    public async Task<IActionResult> GetPost(int postId)
+    public async Task<IActionResult> GetCompletePost(int postId)
     {
         try
         {
@@ -66,6 +70,56 @@ public class PostCompleteController : ControllerBase
         {
             var posts = await _completePostRepository.GetPostByUser(userId);
             return Ok(posts);
+        }
+        catch (Exception e)
+        {
+            throw new Exception($"Error occurred --Message: {e.Message}");
+        }
+    }
+
+    [HttpPost("post")]
+    public IActionResult CreatePost(PostToAddDto post)
+    {
+        try
+        {
+            var postToCreate = _mapper.Map<Post>(post);
+            postToCreate.UserId = int.Parse(this.User.FindFirst("userId")!.Value);
+            postToCreate.PostCreated = DateTime.Now;
+            postToCreate.PostUpdated = DateTime.Now;
+            _completePostRepository.CreatePost(postToCreate);
+            return CreatedAtAction(nameof(GetCompletePost), new {postId = postToCreate.PostId}, postToCreate);
+        }
+        catch (Exception e)
+        {
+            throw new Exception($"Error occurred --Message: {e.Message}");
+        }
+    }
+
+    [HttpPut("update")]
+    public IActionResult UpdatePost(PostToEditDto post)
+    {
+        try
+        {
+            var postToUpdate = _mapper.Map<Post>(post);
+            postToUpdate.PostContent = post.PostContent;
+            postToUpdate.PostTitle = post.PostTitle;
+            postToUpdate.PostUpdated = DateTime.Now;
+
+            return Ok(_completePostRepository.UpdatePost(postToUpdate));
+        }
+        catch (Exception e)
+        {
+            throw new Exception($"Error occurred --Message: {e.Message}");
+        }
+    }
+
+    [HttpDelete("delete/{postId:int:min(1)}")]
+    public IActionResult DeletePost(int postId)
+    {
+        try
+        {
+            var userId = int.Parse(this.User.FindFirst("userId")!.Value);
+            return Ok(_completePostRepository.DeletePost(postId, userId));
         }
         catch (Exception e)
         {
